ea94cbc6ab550f19a5d51381cd3d79e25a069710
Myroslav <myros.photo@gmail.com>
2022-06-27 16:49:43 +0200

    Final

--- sort.js
+++ sort.js
@@ -1,18 +1,19 @@
 let tablica;
 
 function randomNumber() {
-let fake
 
 
-let randomNumber1 = Math.floor((Math.random()*20) +1);
+
+let randomNumber1 = Math.floor((Math.random()*23) +8);
 let randomNumber2 = Math.floor((Math.random()*12)+5);
-let randomNumber3 = Math.floor((Math.random()*13)+1);
-let randomNumber4 = Math.floor((Math.random()*14)+4);
+let randomNumber3 = Math.floor((Math.random()*81)+1);
+let randomNumber4 = Math.floor((Math.random()*90)+4);
 let randomNumber5 = Math.floor((Math.random()*33)+2);
 let randomNumber6 = Math.floor((Math.random()*24)+3);
-let randomNumber7 = Math.floor((Math.random()*40)+2);
+let randomNumber7 = Math.floor((Math.random()*40)+9);
+let randomNumber8 = Math.floor((Math.random()*50)+8);
 
-tablica = [randomNumber1, randomNumber2, randomNumber3, randomNumber4, randomNumber5, randomNumber6, randomNumber7]
+tablica = [randomNumber1, randomNumber2, randomNumber3, randomNumber4, randomNumber5, randomNumber6, randomNumber7, randomNumber8]
 
 console.log("My number " + tablica[0])
 document.getElementById("nieposortowana"). textContent = tablica;
